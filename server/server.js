2d2bb76529f2ae64211dd30215313f242bf37ae7
Mohammed Abbasi <raamizabbasi@Mohammeds-MacBook-Pro.local>
2020-09-12 15:08:53 -0400

    Improved design and added to Settings Page

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = 3000
+const server = app.listen(PORT, () => console.log('Listening on Port: ' + PORT))
+const io = require('socket.io').listen(server)
+
+app.use(express.static(__dirname.replace('/server', '/public')))
+
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname.replace('/server'), '/index.html'))
+})
+
+app.get('/settings', async (req, res) => {
+    res.sendFile(path.join(__dirname.replace('/server', '/public'), '/settings.html'))
+})
+
+let users = 0
+
+io.on('connection', socket => {
+    console.log('A user has connected')
+    users += 1
+    socket.emit('users', users)
+})
